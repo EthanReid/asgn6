bd2e68cb67f59cad777ebba5a60211b3fc898ea6
Ethan Reid <erpreid@gmail.com>
2022-02-24 20:24:18 -0800

    pop and push

--- stack.c
+++ stack.c
@@ -5,12 +5,24 @@
 
 struct Stacks stack;
 
-int push(int Value){
-    stack.index = Value;
+int push(int value){
+    stack.frame[stack.index] = value;
+    stack.index++;
+    return 0;
+};
+
+int pop(int *value){
+    stack.index--;
+    value = stack.frame[stack.index];
     return 0;
 };
 
 int main(){
+    int popVal = 0;
     push(5);
+    printf("%d\n", stack.frame[stack.index-1]);
+    printf("%d\n", stack.index);
+    pop(popVal);
+    printf("%d\n", stack.frame[stack.index]);
     printf("%d\n", stack.index);
 }
